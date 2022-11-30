3e990849dbe547a4d7bc35ab976c111554f19b54
amol2311 <amol.temkar23@gmail.com>
2022-11-29 18:20:19 -0600

    added webview with action bar fix

--- app/src/main/java/edu/cs371m/finalproject/ui/recipe/Recipe.kt
+++ app/src/main/java/edu/cs371m/finalproject/ui/recipe/Recipe.kt
@@ -1,11 +1,18 @@
 package edu.cs371m.finalproject.ui.recipe
 
 
+import android.annotation.TargetApi
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
-import android.webkit.WebSettings
+import android.view.View
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import edu.cs371m.finalproject.databinding.ActivityOneRecipeBinding
 import edu.cs371m.finalproject.ui.MainViewModel
@@ -14,7 +21,44 @@ import edu.cs371m.finalproject.ui.MainViewModel
 class Recipe : AppCompatActivity()
 {
     private lateinit var binding : ActivityOneRecipeBinding
+    private lateinit var webView: WebView
 
+    private inner class CustomChromeClient : WebChromeClient() {
+        private var mCustomView: View? = null
+        private var mCustomViewCallback: CustomViewCallback? = null
+        private var mOriginalOrientation = 0
+        private var mOriginalSystemUiVisibility = 0
+        override fun getDefaultVideoPoster(): Bitmap? {
+            return if (mCustomView == null) {
+                null
+            } else BitmapFactory.decodeResource(applicationContext.resources, 2130837573)
+        }
+
+        override fun onHideCustomView() {
+            (window.decorView as FrameLayout).removeView(mCustomView)
+            mCustomView = null
+            window.decorView.systemUiVisibility = mOriginalSystemUiVisibility
+            requestedOrientation = mOriginalOrientation
+            mCustomViewCallback!!.onCustomViewHidden()
+            mCustomViewCallback = null
+        }
+
+        override fun onShowCustomView(
+            paramView: View?,
+            paramCustomViewCallback: CustomViewCallback?
+        ) {
+            if (mCustomView != null) {
+                onHideCustomView()
+                return
+            }
+            mCustomView = paramView
+            mOriginalSystemUiVisibility = window.decorView.systemUiVisibility
+            mOriginalOrientation = requestedOrientation
+            mCustomViewCallback = paramCustomViewCallback
+            (window.decorView as FrameLayout).addView(mCustomView, FrameLayout.LayoutParams(-1, -1))
+            window.decorView.systemUiVisibility = 3846 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -30,17 +74,31 @@ class Recipe : AppCompatActivity()
 
         binding.title.text = callingBundle?.getCharSequence(MainViewModel.mealNameKey).toString()
 
-        val frameVideo =
-            "<html><body>Video From YouTube<br><iframe width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/nMyBC9staMU\" frameborder=\"0\" allowfullscreen></iframe></body></html>"
+        webView = binding.recipeVideo
 
-        binding.recipeVideo.setWebViewClient(object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                return false
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = CustomChromeClient() // Full-screen support
+        val webSetting = webView.settings
+        webSetting.javaScriptEnabled = true
+        webSetting.allowFileAccess = true
+        webView.webChromeClient = object : WebChromeClient() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun onPermissionRequest(request: PermissionRequest) {
+                request.grant(request.resources)
             }
-        })
-        val webSettings: WebSettings = binding.recipeVideo.getSettings()
-        webSettings.javaScriptEnabled = true
-        binding.recipeVideo.loadData(frameVideo, "text/html", "utf-8")
+        }
+        if (savedInstanceState == null) {
+            webView.loadUrl("https://www.youtube.com/embed/nMyBC9staMU")
+        }
+
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        webView.restoreState(savedInstanceState)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
